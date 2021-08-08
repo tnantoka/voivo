c2fffc444c253ace5a3de654460f03057f004d02
tnantoka <tnantoka@bornneet.com>
2021-08-08 15:18:57 +0900

    feat: add dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(MyApp());
 }
 
